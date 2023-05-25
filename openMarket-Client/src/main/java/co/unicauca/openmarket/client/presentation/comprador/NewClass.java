95221b2995a0dc5ec4594e012795da03a64db007
Naren Imbachi <nimbachi@unicauca.edu.co>
2023-05-25 10:01:29 -0500

    Agregando interfaces Comprador

---
+++ openMarket-Client/src/main/java/co/unicauca/openmarket/client/presentation/comprador/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.unicauca.openmarket.client.presentation.comprador;
+
+/**
+ *
+ * @author Hewlett Packard
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        GUIComprador jp = new GUIComprador();
+        jp.setVisible(true);
+    }
+}
